636a826171aafc677f347dde01bec0c2d9d19802
loonghao <hal.long@outlook.com>
2024-05-16 23:03:43 +0800

    fix: Improve code and fix deploy
    Signed-off-by: loonghao <hal.long@outlook.com>

--- maya_umbrella_scanner/cli.py
+++ maya_umbrella_scanner/cli.py
@@ -1,7 +1,6 @@
 # Import built-in modules
 import argparse
 import os
-import subprocess
 import sys
 from tempfile import TemporaryDirectory
 
@@ -10,9 +9,8 @@ import click
 from maya_umbrella.filesystem import get_maya_install_root
 
 # Import local modules
-from maya_umbrella_scanner.filesystem import assemble_env_paths
 from maya_umbrella_scanner.filesystem import assemble_rg_varius_check_commands
-from maya_umbrella_scanner.template import BAT_TEMPLATE
+from maya_umbrella_scanner.filesystem import create_bat_file
 from maya_umbrella_scanner.template import RUNNER_TEMPLATE
 
 
@@ -29,20 +27,10 @@ def main():
         maya_root = get_maya_install_root(options.maya_version)
         maya_python = os.path.join(maya_root, "bin", "mayapy.exe")
         click.echo(f"Loading maya... {maya_python}")
-        site_packages = os.path.join(os.path.dirname(sys.executable), "lib", "site-packages")
         with TemporaryDirectory() as temp_dir:
             run_maya_py = os.path.join(temp_dir, "run_maya.py")
             with open(run_maya_py, "w") as f:
                 f.write(RUNNER_TEMPLATE)
-            bat_template = BAT_TEMPLATE.format(
-                PYTHONPATH=assemble_env_paths(site_packages),
-                MAYA_PYTHON=maya_python,
-                SCRIPT_PATH=run_maya_py,
-                SCRIPT_ARGS=infected_file,
-            )
-            bat_file = os.path.join(temp_dir, "call_maya.bat")
-            with open(bat_file, "w") as f:
-                f.write(bat_template)
-            subprocess.call(bat_file, shell=True)
+            create_bat_file(maya_python, run_maya_py, infected_file, temp_dir)
     else:
         click.echo(f"Export infected files to: {infected_file}")
